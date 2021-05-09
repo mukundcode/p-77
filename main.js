81b347ea581528d77f5f657172c0a558967bbf37
mukundcode <83866313+mukundcode@users.noreply.github.com>
2021-05-09 17:50:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("my name is mukund");
+
+var name="mukund";
+var name2="yadav";
+
+var full_name=name.concat(name2);
+console.log(full_name);
+
+var a=6;
+var b=4;
+
+var addition=a+b;
+console.log(addition);
+
+var subtraction=a-b;
+console.log(subtraction);
+
+var multiplication=a*b;
+console.log(multiplication);
+
+var division=a/b;
+console.log(division);
